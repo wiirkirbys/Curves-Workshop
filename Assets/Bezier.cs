4db7e4a2aff973e0deba644b148fb9c2916a4386
wiirkirbys <47958857+wiirkirbys@users.noreply.github.com>
2021-04-02 16:49:59 -0700

    Orbits and Beziers implemented

---
+++ Assets/Bezier.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bezier : MonoBehaviour
+{
+
+
+    [SerializeField]
+    private Vector3[] controlPoints;
+    private float t;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        t = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        t = Mathf.PingPong(Time.time, 1);
+        gameObject.transform.position = BezierPoint(controlPoints, t);
+    }
+
+    // https://www.gamasutra.com/blogs/VivekTank/20180806/323709/How_to_work_with_Bezier_Curve_in_Games_with_Unity.php has a good visual for this
+    Vector3 BezierPoint(Vector3[] points, float t)
+    {
+        if (points.Length == 2) // base case: returns the exact point wanted
+        {
+            return Vector3.Lerp(points[0], points[1], t);
+        }
+        else
+        {
+            Vector3[] newPoints = new Vector3[points.Length - 1];
+            for (int i = 0; i < points.Length - 1; i++)
+            {
+                newPoints[i] = Vector3.Lerp(points[i], points[i + 1], t); // construct a new array of Lerp points
+            }
+            return BezierPoint(newPoints, t); // recursion
+        }
+    }
+}
---
+++ Assets/Orbit.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Orbit : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject centerObject;
+    [SerializeField]
+    float xTarget = 0f, yTarget = 0f, zTarget = 0f;
+    public float speed = 3;
+    private Vector3 rotation;
+   
+    private Rigidbody orbiter;
+    // Start is called before the first frame update
+    void Start()
+    {
+        orbiter = gameObject.GetComponent<Rigidbody>();
+        rotation = new Vector3(xTarget, yTarget, zTarget); //will rotate towards the stated vector relative to center
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector3 posToMidLine = Vector3.Normalize(gameObject.transform.position - centerObject.transform.position);
+        Vector3.OrthoNormalize(ref posToMidLine, ref rotation);
+        orbiter.velocity = rotation * speed;
+    }
+}
